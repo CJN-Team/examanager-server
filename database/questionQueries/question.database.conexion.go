357bff8bad04bca6857ab7a4b03aba5aa4858458
Nicolas Raigosa <37982751+nraigosam@users.noreply.github.com>
2020-09-24 11:09:23 -0500

    creacion del modelo de pregntas y crreacion en la base de datos

---
+++ database/questionQueries/question.database.conexion.go
@@ -0,0 +1,48 @@
+package questionqueries
+
+import (
+	"context"
+	"time"
+
+	dbConnection "github.com/CJN-Team/examanager-server/database"
+	"github.com/CJN-Team/examanager-server/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//AddQuestion inserta en la base de datos el modelo de la pregunta
+func AddQuestion (questionModel models.Question) (string, bool, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := dbConnection.MongoConexion.Database("examanager_db")
+	col := db.Collection("Questions")
+
+	result, err := col.InsertOne(ctx, questionModel)
+	if err != nil {
+		return "", false, err
+	}
+
+	QuestionID, _ := result.InsertedID.(primitive.ObjectID)
+	return QuestionID.String(), true, nil
+}
+
+//GetQuestionByID busca en la base de datos la existencia de una pregunta por el ID
+func GetQuestionByID(QuestionID string) (models.Question, bool,error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := dbConnection.MongoConexion.Database("examanager_db")
+	col := db.Collection("Questions")	
+
+	var questionInfo models.Question
+	id,_ := primitive.ObjectIDFromHex(QuestionID)
+	err := col.FindOne(ctx,bson.M{"_id": id}).Decode(&questionInfo)
+
+	if err != nil {
+		return questionInfo, false,err
+	}
+	return questionInfo, true,nil
+}
